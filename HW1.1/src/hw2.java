9a0c9430b0fba47156aa857e23b615c9db232da0
Pavel Shetkov <fertym99@gmail.com>
2022-05-25 16:45:29 +0300

    hello

---
+++ HW1.1/src/hw2.java
@@ -0,0 +1,9 @@
+public class hw2 {
+    private static void main(String[] args){
+        int x = 5;
+        int y = 15;
+        if(Math.pow(x,y) > 1000000000){
+            System.out.println("Ебать!");
+        }
+    }
+}
